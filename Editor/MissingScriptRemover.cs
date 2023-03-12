bef409ebe8fab62134c2a607f826a4985a6a01a9
Favnirrr <83111949+Favnirrr@users.noreply.github.com>
2023-03-12 14:27:00 +0900

    Add files vis upload.

---
+++ Editor/MissingScriptRemover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MissingScriptRemover : EditorWindow
+{
+    [MenuItem("Tools/Remove Missing Scripts")]
+    public static void RemoveMissingScripts()
+    {
+        int count = 0;
+        GameObject[] objects = FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in objects)
+        {
+            if (GameObjectUtility.RemoveMonoBehavioursWithMissingScript(obj) > 0)
+            {
+                count++;
+            }
+        }
+
+        if (count > 0)
+        {
+            Debug.Log($"Removed {count} missing scripts.");
+        }
+        else
+        {
+            Debug.Log("No missing scripts found.");
+        }
+    }
+}
