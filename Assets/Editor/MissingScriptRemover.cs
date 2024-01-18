956db91383d7199e28f0b83cdb3fa11d0a47dc25
Favnirrr <favnirrr224@gmail.com>
2024-01-18 14:42:50 +0900

    UnityProject再作成

---
+++ Assets/Editor/MissingScriptRemover.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MissingScriptRemover : EditorWindow
+{
+    [MenuItem("Tools/Remove Missing Scripts/Scene")]
+    public static void RemoveMissingScriptsOnScene()
+    {
+        int count = 0;
+        GameObject[] objects = FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in objects)
+        {
+            if (GameObjectUtility.RemoveMonoBehavioursWithMissingScript(obj) > 0)
+            {
+                count++;
+            }
+        }
+
+        if (count > 0)
+        {
+            Debug.Log($"Removed {count} missing scripts.");
+        }
+        else
+        {
+            Debug.Log("No missing scripts found.");
+        }
+    }
+
+    [MenuItem("Tools/Remove Missing Scripts/Prefab")]
+    public static void RemoveMissingScriptsOnPrefab()
+    {
+        // 選択したprefabのGUIDを取得
+        string[] guids = Selection.assetGUIDs;
+
+        // 選択したprefabがない場合
+        if (guids.Length == 0)
+        {
+            // エラーダイアログ
+            EditorUtility.DisplayDialog("Error", "No prefab selected.", "OK");
+            // ログ出力
+            Debug.LogWarning("No prefab selected.");
+            return;
+        }
+
+        // 削除したコンポーネントのカウンター
+        int count = 0;
+
+        for(int i=0;i<guids.Length;i++){
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+            GameObject prefabInstance = PrefabUtility.LoadPrefabContents(path);
+            if (prefabInstance != null)
+            {
+                // 子オブジェクトを取得する
+                var parentAndChildren = prefabInstance.GetComponentsInChildren<Transform>(true);
+                foreach(Transform Tsf in parentAndChildren){
+                    if (GameObjectUtility.RemoveMonoBehavioursWithMissingScript(Tsf.gameObject) > 0)
+                    {
+                        count++;
+                    }
+                }
+                PrefabUtility.SaveAsPrefabAsset(prefabInstance, path);
+                PrefabUtility.UnloadPrefabContents(prefabInstance);
+            }
+        }
+        AssetDatabase.Refresh();
+        
+        Debug.Log($"Removed {count} Missing Scripts On Selected Prefab.");
+    }
+}
